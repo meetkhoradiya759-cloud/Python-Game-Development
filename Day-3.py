c5a8b1032b8cb2581a0c55beebb37c5b4a0e532b
meetkhoradiya759-cloud <meetkhoradiya759@gmail.com>
2026-05-14 22:20:27 +0530

    Add files via upload

---
+++ Day-3.py
@@ -0,0 +1,7 @@
+''' 
+    ->  Initialize target
+    ->  Initialize Game Stats (PLAYING , WIN , GAME OVER) (Default = PLAYING)
+    ->  Restart Game AND Quit Game options
+    ->  All 3 game stats algorithms (How Each Game stat Works..)
+    Exercise -> Displaying Score , Time Out System , Shrink Target
+'''
