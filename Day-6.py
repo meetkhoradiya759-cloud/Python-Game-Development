9373ac9497ececceb89d6ca25755b2b1bb13ba78
MeetK-Dev <meetkhoradiya759@gmail.com>
2026-05-18 18:06:29 +0530

    Add files via upload

---
+++ Day-6.py
@@ -0,0 +1,18 @@
+'''
+Bulding Snake game 
+To create game : New Folder is created 'Snake Game'
+Snake Game/ :
+    |------ main.py         <--- game loop , states
+    |------ snake.py        <--- Snake Class
+    |------ food.py         <--- Food Class
+    |------ settings.py     <--- All Constants
+    |------ highscore.txt   <--- Created automatically on first run
+'''
+
+"""
+    settings.py    --->    31  Lines
+    snake.py       --->    110 Lines
+    food.py        --->    38  Lines
+    main.py        --->    167 Lines
+    TOTAL          --->    367 Lines
+"""
